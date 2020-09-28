05fbab1caa8434d05ab00776fa6d327222ba6820
cosimo chellini <cosimo.chellini@gmail.com>
2020-09-28 18:31:59 +0200

    feat(tailwind): added tailwind

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
@@ -26,5 +26,8 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+body {
+  @apply bg-gray-200;
 }
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,19 +1,30 @@
 <template>
-  <div>
-    <form>
-      <label>Name</label>
-      <input type="text" v-model="form.name" />
-      <label>Pin</label>
-      <input type="text" v-model="form.pin" />
-      <button @click.prevent="login">login</button>
+  <div class="w-full max-w-xl h-full m-auto">
+    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <label class="label">Name</label>
+      <input
+        type="text"
+        v-model="form.name"
+        class="inputForm"
+        autocomplete="email"
+      />
+      <label class="label">Pin</label>
+      <input
+        type="tel"
+        v-model="form.pin"
+        class="inputForm"
+        autocomplete="current-password"
+      />
+      <button @click.prevent="login" class="loginButton">login</button>
     </form>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { isAuthorized, login } from "@/services/authService";
 import { roleConstants } from "@/constants/roleConstants";
+import { isAuthorized, login } from "@/services/authService";
+
 export default defineComponent({
   data() {
     return {
@@ -41,5 +52,29 @@ export default defineComponent({
 });
 </script>
 
-<style>
+<style scoped>
+.label {
+  @apply block text-gray-700 text-sm font-bold mb-2 text-left;
+}
+
+.inputForm {
+  @apply shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight mb-2;
+}
+
+.inputForm:focus {
+  @apply shadow-outline outline-none;
+}
+
+.loginButton {
+  @apply bg-gradient-to-l from-teal-400 to-blue-500 m-2 text-white font-bold py-2 px-4 rounded;
+}
+
+.loginButton:hover {
+  @apply bg-blue-700;
+}
+
+.loginButton:focus {
+  @apply shadow-outline outline-none;
+}
+
 </style>
