daa07ca2e4ea7bea803f4ca84cf84aac90e4000c
cosimo chellini <cosimo.chellini@gmail.com>
2021-03-09 14:55:39 +0100

    fix(bottom-bar): added more reactivity

--- src/components/base/BottomBar.vue
+++ src/components/base/BottomBar.vue
@@ -5,11 +5,17 @@
       <div class="flex justify-between">
         <router-link
           class="w-full focus:text-teal-500 hover:text-teal-500 justify-center inline-block text-center pt-2 pb-1"
+          :class="isActive(element.name) ? element.activeColor : ''"
           v-for="element in currentButtonBar"
           :key="element.name"
           :to="{ name: element.route }"
         >
-          <i :class="`fa-${element.icon}`" class="fas fa-lg"></i>
+          <i
+            v-if="isActive(element.name)"
+            :class="element.iconActive"
+            class="fa-lg"
+          ></i>
+          <i v-else :class="`fa-${element.icon}`" class="fas fa-lg"></i>
           <span class="tab tab-home block text-sm capitalize tracking-wider">
             {{ $t(`bottom.route.${element.name}`) }}
           </span>
@@ -21,7 +27,13 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-type BottonElement = { name: string; icon: string; route: string };
+type BottonElement = {
+  name: string;
+  icon: string;
+  route: string;
+  activeColor: string;
+  iconActive: string;
+};
 
 const routes: { game: string; elements: BottonElement[] }[] = [
   {
@@ -29,11 +41,25 @@ const routes: { game: string; elements: BottonElement[] }[] = [
     elements: [
       {
         name: "history",
-        icon: "history",
+        icon: "fas fa-history",
+        iconActive: "fas fa-plus-circle",
         route: "trumpHistory",
+        activeColor: "text-blue-700",
       },
-      { name: "new", icon: "plus-circle", route: "trumpNew" },
-      { name: "stats", icon: "chart-bar", route: "trumpStats" },
+      {
+        name: "new",
+        icon: "fas fa-plus",
+        iconActive: "fas fa-plus-circle",
+        route: "trumpNew",
+        activeColor: "text-blue-700",
+      },
+      {
+        name: "stats",
+        icon: "far fa-chart-bar",
+        iconActive: "fas fa-chart-bar",
+        route: "trumpStats",
+        activeColor: "text-blue-700",
+      },
     ],
   },
   {
@@ -41,11 +67,25 @@ const routes: { game: string; elements: BottonElement[] }[] = [
     elements: [
       {
         name: "history",
-        icon: "history",
+        icon: "fas fa-history",
+        iconActive: "fas fa-plus-circle",
+        activeColor: "text-red-800",
         route: "secretHitlerHistory",
       },
-      { name: "new", icon: "plus-circle", route: "secretHitlerNew" },
-      { name: "stats", icon: "chart-bar", route: "secretHitlerStats" },
+      {
+        name: "new",
+        icon: "fas fa-plus-circle",
+        iconActive: "fas fa-plus-circle",
+        route: "secretHitlerNew",
+        activeColor: "text-red-800",
+      },
+      {
+        name: "stats",
+        icon: "fas fa-chart-bar",
+        iconActive: "fas fa-plus-circle",
+        route: "secretHitlerStats",
+        activeColor: "text-red-800",
+      },
     ],
   },
 ];
@@ -62,6 +102,11 @@ export default defineComponent({
   computed: {
     currentButtonBar(): BottonElement[] {
       return this.routes.find((r) => r.game == this.game)?.elements ?? [];
+    },
+  },
+  methods: {
+    isActive(name: string) {
+      return this.$route.matched.some((m) => m.path.includes(name));
     },
   },
 });
