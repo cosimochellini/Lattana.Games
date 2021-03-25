6a6ccec5c773003d13008cf4c52e682d6f688fb0
cosimo chellini <cosimo.chellini@gmail.com>
2021-03-25 09:15:00 +0100

    fix(purge-css): updated purge settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: { content: ["./public/**/*.html", "./src/**/*.vue"] },
+  purge: { content: ["./public/**/*.html", "./src/**/*.{vue,ts}"] },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
