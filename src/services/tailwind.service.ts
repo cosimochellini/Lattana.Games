0e6b3a63137b54a50419a8924e8b4c7bfab7ad93
cosimo chellini <cosimo.chellini@gmail.com>
2021-03-29 14:26:37 +0200

    feat(service): moved more stuff to the specific service

--- src/services/tailwind.service.ts
+++ src/services/tailwind.service.ts
@@ -10,6 +10,23 @@ export const tailwind = {
     return ratio >= 0.5 !== reverse ? "bg-green-300" : "bg-red-300";
   },
 
+  bindRate(rate: number): string {
+    if (rate < 0.16) return "bg-green-400";
+    if (rate < 0.33) return "bg-green-300";
+    if (rate < 0.5) return "bg-green-200";
+
+    if (rate < 0.66) return "bg-red-200";
+    if (rate < 0.83) return "bg-red-300";
+
+    return "bg-red-400";
+  },
+
+  text(condition: boolean) {
+    return condition ? "text-green-900" : "text-red-900";
+  },
+
+  rankingBackground: ["ring-yellow-400", "ring-gray-300", "ring-yellow-700"],
+
   trump: {
     background(match: trumpMatch) {
       return match.finalScore >= match.startingScore
@@ -17,9 +34,7 @@ export const tailwind = {
         : "bg-red-200";
     },
     text(match: trumpMatch) {
-      return match.finalScore >= match.startingScore
-        ? "text-green-900"
-        : "text-red-900";
+      return tailwind.text(match.finalScore >= match.startingScore);
     },
   },
 
--- src/views/secretHitler/rankings/Index.vue
+++ src/views/secretHitler/rankings/Index.vue
@@ -24,20 +24,20 @@
     </div>
     <div
       class="flex flex-col justify-center px-3 py-3 bg-white border border-gray-300 rounded-xl my-1 mx-2 max-w-2xl text-center lg:m-auto lg:mt-1"
-      v-for="({ profile, rank }, index) in sortedRanks"
-      :key="profile.nickname"
+      v-for="(rank, index) in sortedRanks"
+      :key="rank.profile.nickname"
     >
       <div class="grid grid-cols-4 items-center text-center">
         <img
           class="w-10 h-10 rounded-full col-span-1 m-auto"
-          :src="image(profile.profileImage, 500)"
+          :src="image(rank.profile.profileImage, 500)"
           :class="bindImageRing(index)"
-          :title="profile.name + ' ' + profile.surname"
+          :title="rank.profile.name + ' ' + rank.profile.surname"
         />
         <span class="col-span-2 m-auto">
           <span class="text-gray-700 font-semibold font-sans tracking-wide">
-            {{ profile.name }}
-            {{ profile.surname }}
+            {{ rank.profile.name }}
+            {{ rank.profile.surname }}
           </span>
         </span>
         <span class="col-span-1 m-auto">
@@ -60,34 +60,25 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
 import { Dictionary } from "@/types";
+import { defineComponent } from "vue";
+import { orderby } from "@/types/ranking";
 import { image } from "@/instances/sanity";
 import { byNumber, byValue } from "sort-es";
 import { secretHitlerMatch } from "@/types";
 import Badge from "@/components/base/Badge.vue";
 import { groq } from "@/utils/GroqQueryBuilder";
-import { secretHitlerMatchPlayer } from "@/types";
-import { orderby, information } from "@/types/ranking";
+import { tailwind } from "@/services/tailwind.service";
+import { sanityTypes } from "@/constants/roleConstants";
 import { notification } from "@/services/notification.service";
+import { RankingList } from "@/utils/classes/stats/ranks/baseRank";
 import { orderbyDirection, secretHitlerOrderBy } from "@/types/ranking";
-import { sanityTypes, secretHitlerRole } from "@/constants/roleConstants";
 import { percentageFormatter, smallNumberFormatter } from "@/utils/formatters";
+import { secretHitlerRank } from "@/utils/classes/stats/ranks/secretHitlerRank";
 
 const matchesQuery = new groq.QueryBuilder(sanityTypes.secretHitlerMatch)
   .select("..., players[] -> { player ->, ...}")
   .cached();
-
-const background = ["ring-yellow-400", "ring-gray-300", "ring-yellow-700"];
-
-declare type secretHitlerInformation = {
-  rank: {
-    hitlerMatches: number;
-    fascistMatches: number;
-    liberalMatches: number;
-    weightedAverage: number;
-  };
-} & information;
 
 const allOrderBy = { ...orderby, ...secretHitlerOrderBy };
 
@@ -96,17 +87,6 @@ const reverseOrderBy = [
   allOrderBy.hitlerMatches,
   allOrderBy.fascistMatches,
 ];
-
-const bindMedia = (...filteredMatches: secretHitlerMatchPlayer[][]) =>
-  (filteredMatches
-    .map((matches) => {
-      const wins = matches.filter((x) => x.win).length;
-
-      return wins ? wins / matches.length || 0 : 0;
-    })
-    .reduce((initial, current) => initial + (current || 0), 0) /
-    filteredMatches.length) *
-  100;
 
 export default defineComponent({
   components: { Badge },
@@ -139,7 +119,7 @@ export default defineComponent({
 
       return {
         ring: true,
-        [background[realIndex]]: true,
+        [tailwind.rankingBackground[realIndex]]: true,
       };
     },
     bindBadgeColor(index: number): string {
@@ -147,21 +127,12 @@ export default defineComponent({
 
       const rate = realIndex / this.sortedRanks.length;
 
-      if (rate < 0.16) return "bg-green-400";
-      if (rate < 0.33) return "bg-green-300";
-      if (rate < 0.5) return "bg-green-200";
-
-      if (rate < 0.66) return "bg-red-200";
-      if (rate < 0.83) return "bg-red-300";
-
-      return "bg-red-400";
+      return tailwind.bindRate(rate);
     },
     bindBadgeTextColor(index: number): string {
       const realIndex = this.bindRealIndex(index, true);
 
-      return realIndex / this.sortedRanks.length < 0.5
-        ? "text-green-800"
-        : "text-red-800";
+      return tailwind.text(realIndex / this.sortedRanks.length < 0.5);
     },
   },
   activated() {
@@ -171,70 +142,16 @@ export default defineComponent({
       .catch(notification.warning);
   },
   computed: {
-    ranking(): secretHitlerInformation[] {
-      const ranks = [] as secretHitlerInformation[];
-
-      for (const nickname of this.allPlayers) {
-        const playerFn = (player: secretHitlerMatchPlayer) =>
-          player.player.nickname === nickname;
-
-        const playerMatches = this.matches
-          .map((match) => match.players.find(playerFn))
-          .filter(Boolean) as secretHitlerMatchPlayer[];
-
-        const profile = playerMatches[0].player;
-
-        const win = playerMatches.filter((player) => player.win).length;
-
-        const hitlerMatches = playerMatches.filter(
-          (player) => player.role === secretHitlerRole.hitler
-        );
-
-        const liberalMatches = playerMatches.filter(
-          (player) => player.role === secretHitlerRole.liberal
-        );
-
-        const fascistMatches = playerMatches.filter(
-          (player) => player.role === secretHitlerRole.fascist
-        );
-
-        const lost = playerMatches.length - win;
-
-        const ratio = win / playerMatches.length;
-
-        const rank = {
-          win,
-          lost,
-          ratio,
-          hitlerMatches: hitlerMatches.length,
-          liberalMatches: liberalMatches.length,
-          fascistMatches: fascistMatches.length,
-          totalMatches: playerMatches.length,
-          totalMatchesYo: playerMatches,
-          weightedAverage: bindMedia(liberalMatches, [
-            ...fascistMatches,
-            ...hitlerMatches,
-          ]),
-        };
-
-        ranks.push({ rank, profile });
-      }
-      return ranks;
+    rankingList(): secretHitlerRank[] {
+      return new RankingList(this.matches, secretHitlerRank.create).toList();
     },
-    allPlayers(): string[] {
-      return [
-        ...new Set(
-          this.matches.flatMap((x) => x.players.map((p) => p.player.nickname))
-        ),
-      ];
-    },
-    sortedRanks(): secretHitlerInformation[] {
+    sortedRanks(): secretHitlerRank[] {
       const desc = this.selectedOrderbyDirection === orderbyDirection.desc;
       const type = this.selectedOrderby;
 
-      return this.ranking
+      return this.rankingList
         .concat()
-        .sort(byValue(({ rank }) => rank[type], byNumber({ desc })));
+        .sort(byValue((rank) => rank[type], byNumber({ desc })));
     },
   },
 });
