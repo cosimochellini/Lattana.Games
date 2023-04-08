dcc980adc3bb62eb912db2f21996619782c06b77
Cosimo Chellini <c.chellini@treedom.net>
2023-04-08 11:03:28 +0200

    feat(deps): updated major dependencies

--- src/composable/timedOpen.ts
+++ src/composable/timedOpen.ts
@@ -1,20 +1,20 @@
-import { ref, watch } from "vue";
+import {ref, watch} from "vue";
 
 export const useTimedOpen = (
-  timeInterval: number = 2000,
-  startOpen: boolean = false
+    timeInterval: number = 2000,
+    startOpen: boolean = false
 ) => {
-  let interval: number;
-  const isOpen = ref(startOpen);
+    let interval: NodeJS.Timer;
+    const isOpen = ref(startOpen);
 
-  const toggle = () => {
-    isOpen.value = !isOpen.value;
-  };
+    const toggle = () => {
+        isOpen.value = !isOpen.value;
+    };
 
-  watch(isOpen, (value) =>
-    value
-      ? (interval = setTimeout(() => (isOpen.value = false), timeInterval))
-      : clearTimeout(interval)
-  );
-  return { isOpen, toggle };
+    watch(isOpen, (value) =>
+        value
+            ? (interval = setTimeout(() => (isOpen.value = false), timeInterval))
+            : clearTimeout(interval)
+    );
+    return {isOpen, toggle};
 };
